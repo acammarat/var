e6cabeca2a74e38ef7a612e0c9039bfe1286b51a
Antonio Cammarata <124535593+acammarat@users.noreply.github.com>
2025-02-13 12:17:06 +0100

    Update scf_band_ci_cc.py

--- scf_band_ci_cc.py
+++ scf_band_ci_cc.py
@@ -18,12 +18,13 @@ cell.build(
          [1.77549387821, 0.00000000000, 1.77549387821],
          [1.77549387821, 1.77549387821, 0.00000000000]],
     atom = 'C 0 0 0',  # in Angstrom
-    basis = '321g',
-  ke_cutoff = 550,
-  mesh = [10]*3,
-  space_group_symmetry = True,
-  symmetry = True,
-  verbose = 5
+#    basis = 'gth-cc-qzvp', #'631g',
+    basis = '321g', #'631g',
+    ke_cutoff = 550,
+    mesh = [10]*3,
+    space_group_symmetry = True,
+    symmetry = True,
+    verbose = 5
 )
 
 
@@ -37,10 +38,10 @@ print('kp = ', kp)
 
 cscf_set = pyscf.pbc.scf.KRHF(cell,kpts=kpts)
 # https://pyscf.org/user/pbc/scf.html#smearing
-# cscf_set = pyscf.pbc.scf.addons.smearing_(cscf_set, sigma=0.0001, method='fermi')
+cscf_set = pyscf.pbc.scf.addons.smearing_(cscf_set, sigma=0.0001, method='fermi')
 
 cscf = cscf_set.run(
-    init_guess = 'chk', # chk to read from chkfile
+    init_guess = 'minao', # chk to read from chkfile
     chkfile    = 'scf_KRHF.chk',
     max_memory = 250000,
     verbose    = 4,
@@ -53,6 +54,20 @@ cscf = cscf_set.run(
     direct_scf = True
 )
 
+# Code to print eigenvalues and occupancy for each k-point in blocks with k-point vector components
+mo_energy_kpts = cscf.mo_energy
+mo_occ_kpts = cscf.mo_occ
+
+for k, (mo_energy, mo_occ) in enumerate(zip(mo_energy_kpts, mo_occ_kpts)):
+    kpt_vector = kpts.kpts[k]  # Correct way to access k-point vector
+    print(f"k-point {k+1} ({kpt_vector[0]:.4f}, {kpt_vector[1]:.4f}, {kpt_vector[2]:.4f}):")
+    print(f"{'Eigenvalues':<20} {'Occupancy':<20}")
+    for energy, occ in zip(mo_energy, mo_occ):
+        print(f"{energy:<20} {occ:<20}")
+    print()
+    
+
+
 #cisolver = pyscf.pbc.ci.KCIS(cscf)
 #cisolver.run(
 #    threads = 30,
@@ -60,6 +75,9 @@ cscf = cscf_set.run(
 #    with_soc = True,
 #)
 
+# Disable smearing and recalculate occupation numbers
+cscf.smearing_method = False
+cscf.mo_occ = cscf.get_occ()
 
 ccsolver = pyscf.pbc.cc.KRCCSD(cscf)
 ccsolver.kpts = kpts.kpts  # Ensure kpts are correctly set
@@ -68,7 +86,7 @@ ccsolver.run(
     diis_space = 8,
     verbose = 4,
     with_soc = True,
-    max_cycle = 100
+    max_cycle = 2
 )
 
 ##################################
@@ -105,7 +123,43 @@ band_kpts, kpath, sp_points = get_bandpath([G, X, W, K, G, L, U, W, L, K, U, X],
 
 band_kpts = cell.get_abs_kpts(band_kpts)
 
-e_kn = cscf.get_bands(band_kpts)[0]
+#e_kn = cscf.get_bands(band_kpts)[0]
+# Replace line 107 with the following code
+e_kn = []
+for kpt in band_kpts:
+    # Update k-points in ccsolver
+    ccsolver.kpts = [kpt]
+    # Run the coupled-cluster solver for the specific k-point
+    ccsolver.run()
+    # Get the band energies for this k-point
+    e_kn.append(ccsolver.mo_energy[0])
+
+# Continue with the rest of the code
+Ef = cscf.get_fermi()
+print("Fermi level: ", Ef)
+e_kn_shift = [en - Ef for en in e_kn]
+
+au2ev = 27.21138624598
+
+# Band structure plot
+nbands = cell.nao_nr()
+# Write bandstructure to file band.dat
+
+outf = "band.dat"
+out = open(outf,"w")
+
+print("Writing band structure to file ", outf)
+
+print("# Fermi level is set to 0; energy levels in eV", file=out)
+print("# G, X, W, K, G, L, U, W, L, K, U, X", file=out)
+print("#", *sp_points, file=out)
+
+for i in range(nbands):
+    for j in range(len(kpath)):
+        print(kpath[j], e_kn_shift[j][i] * au2ev, file=out)
+    print(file=out)
+
+out.close()
 
 Ef = cscf.get_fermi()
 print("Fermi level: ", Ef)
@@ -134,5 +188,4 @@ for i in range(nbands):
     print(file=out)
 
 out.close()
-
 
